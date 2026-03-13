0dfe6cd17529228617af50478edfe0ca7243a849
Jairus Mark <jairuscollantes0@gmail.com>
2026-03-13 21:58:19 +0800

    aiyeeee

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import tkinter as tt
 
 main = tt.Tk()
 main.title("web")
-main.geometry("1200x800")
+main.geometry("400x300")
+label = tt.Label(main, text="wassup brobro!")
+label.pack()
+
 main.mainloop()
